b676068df683b5c59ae021e36e6e906877e2d297
dhsathiya <devarshisathiya5@gmail.com>
2019-05-18 05:46:41 +0530

    Update setup removed unwanted lines

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-echo "use cheat -h for help"
-echo "----------------------------------------------Notice---------------------------------------------"
-echo "| by default delete(-d parameter) is disabled to stop accidental deletion.                      |"
-echo "| to enable uncomment the code in delete option in the file.                                    |"
-echo "| parameter validation is to be set so please use with care otherwise you won't see your cheats |"
-echo "-------------------------------------------------------------------------------------------------"
 wget https://raw.githubusercontent.com/dhsathiya/cheat/master/cheat
 sudo chmod +x cheat
 sudo mv cheat /bin/
 mkdir ~/.cheat
-
+echo "use cheat -h for help"
+echo "----------------------------------------------Notice---------------------------------------------"
+echo "| parameter validation is to be set so please use with care otherwise you won't see your cheats |"
+echo "-------------------------------------------------------------------------------------------------"
